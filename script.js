2a5aaddfddda6bd3f9b0ea60869b19fc58dc58c8
Robert Baboi <robert.baboi@gmail.com>
2018-10-27 21:40:13 +0100

    BUGFIX Continue watching youtube

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto click continue watch on youtube
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/watch?v=qWZL5RnfgtI
+// @grant        none
+// @include      https://www.youtube.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function(){document.getElementById("confirm-button").children[0].children[0].children[0].click()}, 1000);
+})();
